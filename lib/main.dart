9e15b09bd503d8972588baf8557e9c1c0afaa8cf
Jake <16878256+Smartiiez@users.noreply.github.com>
2022-01-17 20:32:57 +0100

    update firebase test app

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-import 'dart:async';
 import 'dart:convert';
-
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "",
+    appId: "",
+    messagingSenderId:
+        "",
+    projectId:
+        "",
+  ));
   runApp(const MyApp());
 }
 
@@ -18,35 +25,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Firebase test',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Firebase test'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,26 +44,31 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late StreamSubscription<DatabaseEvent> _userStream;
   late DatabaseReference _usersDatabase;
-  int _counter = 0;
+  String _firstName = "";
   @override
   void initState() {
-    _usersDatabase = FirebaseDatabase.instance.ref("user");
-    _listenToUserDatabase("*********"); // Replace **** by user child key
+    final FirebaseDatabase usersDatabase = FirebaseDatabase.instance;
+    _usersDatabase = usersDatabase.ref("user");
     super.initState();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  void dispose() {
-    _userStream.cancel();
-    super.dispose();
+  void _incrementCounter() async {
+    try {
+      DataSnapshot snapshot =
+          await _usersDatabase.child("-MqIm1T1ic-8inERbOC5").get();
+      if (snapshot.value != null) {
+        String strValue = jsonEncode(snapshot.value);
+        Map<String, dynamic> value = jsonDecode(strValue);
+        setState(() {
+          _firstName = value["first_name"];
+        });
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print("error $e");
+      }
+    }
   }
 
   @override
@@ -88,10 +82,10 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Fetch firstname of user from Firebase RTDB:',
             ),
             Text(
-              '$_counter',
+              _firstName,
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
@@ -103,24 +97,5 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  void _listenToUserDatabase(String key) async {
-    // I get the issue with the get() method and with the onValue.listen() method
-
-    DataSnapshot test = await _usersDatabase.child(key).get();
-    print(test);
-
-    // _userStream = _usersDatabase.child(key).onValue.listen((event) {
-    //   if (event.snapshot.value != null) {
-    //     final String source = jsonEncode(event.snapshot.value);
-    //     final Map<String, dynamic> json = jsonDecode(source);
-    //     print(json);
-    //   }
-    // }, onError: (e) {
-    //   print("Listen to user database error $e");
-    // }, onDone: () {
-    //   print("listen done");
-    // });
   }
 }
